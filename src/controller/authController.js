12bafa4d084839ffe40a43d30322aba8f6cc39dd
Gaius Emmanuel <gaiusemmanuel12@gmail.com>
2026-08-03 11:05:28 +0100

    create: logout controller function and route

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -83,4 +83,15 @@ const login = async (req, res) => {
   });
 };
 
-export { register, login };
+const logout = async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    status: "success",
+    message: "logged out successfully",
+  });
+};
+
+export { register, login, logout };
