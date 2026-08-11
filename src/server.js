5822863271f21b92d2345b99601c8db17307bee7
Gaius Emmanuel <gaiusemmanuel12@gmail.com>
2026-08-11 01:35:25 +0100

    create: watchlist route and controller

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { connectDB, disconnectDB } from "./config/db.js";
 // import routes
 import movieRoutes from "./routes/movieRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import watchlistRoutes from "./routes/watchlistRoutes.js"
 
 connectDB();
 
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // API Routes
 app.use("/movies", movieRoutes);
 app.use("/auth", authRoutes);
+app.use("/watchlist", watchlistRoutes);
 
 const PORT = 5001;
 
