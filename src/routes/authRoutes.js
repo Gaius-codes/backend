67c765c167ceeb8e049c3394b502e8da7f9d4a42
Gaius Emmanuel <gaiusemmanuel12@gmail.com>
2026-08-14 11:46:13 +0100

    add validation for adding movies and editting movies

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { register, login, logout } from "../controller/authController.js";
 import { loginSchema, registerSchema } from "../validators/authValidators.js";
+import validateRequest from "../middleware/validateRequest.js";
 
 const router = express.Router();
 
-router.post("/register", registerSchema, register);
-router.post("/login", loginSchema, login);
+router.post("/register", validateRequest(registerSchema), register);
+router.post("/login", loginSchema(loginSchema), login);
 router.post("/logout", logout);
 
 export default router;
