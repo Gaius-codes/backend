93bbc71a85da420a1fac1d540f8eee6356495fc0
Gaius Emmanuel <gaiusemmanuel12@gmail.com>
2026-08-14 11:31:13 +0100

    implement validation for registering and login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login, logout } from "../controller/authController.js";
+import { loginSchema, registerSchema } from "../validators/authValidators.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerSchema, register);
+router.post("/login", loginSchema, login);
 router.post("/logout", logout);
 
 export default router;
